5c8201e74949a6966d1238d7ca80492052afb501
Praphan Theeravanvilai <praphan@angstromsolutions.com>
2017-10-27 12:56:31 +0700

    Mac Virtualbox

---
+++ pc-lib2.rb
@@ -0,0 +1,15 @@
+def pcGetContainers(host)
+  `#{$sshCmd} #{hostCmd(host)} docker ps -a`.split(/\n/)[1..-1]
+end
+
+def pcGetLogs(host, container_id)
+  `#{$sshCmd} #{hostCmd(host)} docker logs #{container_id}`
+end  
+
+def pcRunning?(host)
+  `#{$sshCmd} #{hostCmd(host)} pwd` =~ /^\/home\/praphan/
+end
+
+def pcStop(host)
+  `#{$sshCmd} #{hostCmd(host)} sudo poweroff`
+end
---
+++ ssh-lib2.rb
@@ -0,0 +1,29 @@
+$sshCmd = 'ssh -q  -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no'
+
+def hostCmd(host)
+  if host.downcase.include? 'azure'
+    "praphan@ppt-#{host}.ddns.net"
+  elsif host.downcase == 'pc'
+    "praphan@ppt-pc.ddns.net -p 2625"
+  elsif host.downcase == 'mac'
+    "praphan@ppt-mac.ddns.net -p 2625"
+  else
+    "-i ~/Dropbox/booking/Docker/ntp.pem ubuntu@ppt#{host.downcase.gsub('aws','')}.ddns.net"
+  end
+end
+
+def d_stop(host)
+  dockerStopCmd = "'docker stop $(docker ps -qa) ; docker rm $(docker ps -qa)'"
+  stopTmux = 'tmux kill-server'
+  
+  `#{$sshCmd} #{hostCmd(host)} #{dockerStopCmd}`
+  `#{$sshCmd} #{hostCmd(host)} #{stopTmux}`  
+end
+
+def tmuxNewSession(session)
+  "tmux new-session -d -s t#{session}"
+end
+
+def tmuxSession(session)
+  "tmux send-keys -t t#{session}"
+end
