da4fd4bc3ce9fbaa0d3fc01278bb5fcf7eed349a
Praphan Theeravanvilai <theepraphan@gmail.com>
2019-10-20 20:20:23 +0700

    Update check-aws.rb
    minor bug

--- check-aws.rb
+++ check-aws.rb
@@ -11,8 +11,8 @@ end
 for i in awsGetRunning() do
   puts "aws#{i} running"
   for s in awsGetContainers(i) do
-    File.open("#{$dir_name}/aws#{i}-#{containerID(s)}", 'w') { |file| file.write(awsGetLogs(i,containerID(s))) }
     puts "aws#{i}-#{containerID(s)}" 
     puts awsGetLogs(i,containerID(s))
+    puts
   end
 end
