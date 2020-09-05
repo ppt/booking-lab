bfa6bcacbf99aa73a4ce596fa2643b0f0ec38961
Praphan Theeravanvilai <theepraphan@gmail.com>
2020-09-05 08:54:35 +0700

    rm aws in .gitignore to git aws-lib

---
+++ aws-lib2.rb
@@ -0,0 +1,38 @@
+def awsStatus(id)
+  `bash -c 'source ~/dotFiles/aws.sh; aws-status #{id}'`[0..-2]
+end
+
+
+def awsIsRunning(id)
+  awsStatus(id) != ''
+end
+
+def awsStart(id)
+  `bash -c 'source ~/dotFiles/aws.sh; aws-start #{id}'`
+end
+
+def awsStop(id)
+  `bash -c 'source ~/dotFiles/aws.sh; aws-stop #{id}'`
+end
+
+def awsTerminate(id)
+  `bash -c 'source ~/dotFiles/aws.sh; aws-terminate #{id}'`
+end
+
+def awsGetRunning()
+  result = []
+  for i in 1..12 do
+    if awsIsRunning(i)
+      result.push i
+    end
+  end
+  return result
+end
+
+def awsGetContainers(aws_id)
+  `ssh -q -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -i ~/Dropbox/booking/Docker/ntp.pem ubuntu@ppt#{aws_id}.ddns.net docker ps -a`.split("\n")[1..-1]
+end
+
+def awsGetLogs(aws_id, container_id)
+  `ssh -q -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -i ~/Dropbox/booking/Docker/ntp.pem ubuntu@ppt#{aws_id}.ddns.net docker logs #{container_id}`
+end
