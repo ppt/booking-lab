07b246965870b9821557108a60fa34cd111db30a
Praphan Theeravanvilai <theepraphan@gmail.com>
2020-09-07 17:04:37 +0700

    gen booking4

--- gen.rb
+++ gen.rb
@@ -24,9 +24,9 @@ passwd = YAML.load_file passwd
 
 def runCmd(user, passwd, course, seq, starttime,host,session)
   if host.downcase.include? 'aws'
-    "setsid /home/ubuntu/booking2/booking2.js --user=#{user} --password=#{passwd} --seq=#{seq} --class-name='#{course.sub(' ','Space')}' --start-time='#{starttime}' >#{host}-#{session} 2>&1 &"
+    "setsid /home/ubuntu/booking2/booking4.js --user=#{user} --password=#{passwd} --seq=#{seq} --class-name='#{course.sub(' ','Space')}' --start-time='#{starttime}' >#{host}-#{session} 2>&1 &"
   else
-    "nohup ~/booking2/booking2.js --user=#{user} --password=#{passwd} --seq='#{seq}' --class-name='#{course.sub(' ','Space')}' --start-time='#{starttime}' >#{host}-#{session} 2>&1 &"
+    "nohup ~/booking2/booking4.js --user=#{user} --password=#{passwd} --seq='#{seq}' --class-name='#{course.sub(' ','Space')}' --start-time='#{starttime}' >#{host}-#{session} 2>&1 &"
   end
 end
 
